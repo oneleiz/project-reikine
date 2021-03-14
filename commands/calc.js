35016f9c094546821f5e82dadd71ca1da87b1fb0
oneleiz <80594569+oneleiz@users.noreply.github.com>
2021-03-15 01:35:00 +0800

    added support for rounding & square root
    working on support for multiple arguments

--- commands/calc.js
+++ commands/calc.js
@@ -18,10 +18,22 @@ module.exports.run = async (bot, message, args) => {
     var equal = parseFloat(args[0]) / parseFloat(args[2]);
     return message.channel.send(equal);
 
-  } else if (args[1] === "%")
-    var equal = parseFloat(args[0]) % parseFloat(args[2]);
+  } else if (args[1] === "%") {
+      var equal = parseFloat(args[0]) % parseFloat(args[2]);
     return message.channel.send(equal);
-  
+
+  } else if (args[1] === "rnd") {
+      var equal = Math.floor(Math.round(parseFloat(given)))
+    return message.channel.send(equal)
+
+  } else if (args[1] === "sqrt") {
+      var equal = Math.floor(Math.sqrt(parseFloat(given)))
+      return message.channel.send(equal)
+
+  } else if (!given) { return message.channel.send('specify an operation eg. (1 + 2)')
+  } else if (args[3]) { return message.channel.send('cannot compute multiple arguments at once')
+  } else return message.channel.send('invalid operation')
+
 } 
 
 module.exports.help = {
