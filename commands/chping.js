1a40197378cc2f643136e6a2b5ba2394177d4e9d
oneleiz <80594569+oneleiz@users.noreply.github.com>
2021-03-15 01:36:56 +0800

    replaced variables
    looking into a more accurate way of checking somebody's ping

--- commands/chping.js
+++ commands/chping.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`${message.createdTimestamp - Date.now()}ms`);
+    message.channel.send(`${Math.round(parseFloat(bot.ws.ping))}ms`);
 }
 
 module.exports.help = {
